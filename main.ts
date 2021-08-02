8ab25d840185bd3a2bbe25f906e82f59bf3c409b
some8a <67367308+some8a@users.noreply.github.com>
2021-08-02 12:10:47 +0900

    更新 pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,123 @@
- 
+namespace SpriteKind {
+    export const kagu = SpriteKind.create()
+    export const king = SpriteKind.create()
+}
+function spritesetting () {
+    bed = sprites.create(img`
+        ...bbccccccbb...
+        ..bdddddddd1db..
+        .bddbbbbbbbbddb.
+        .cdb11111111bdc.
+        .cbcdbbbbbbdcbc.
+        .fbcc111111ccbf.
+        .fbcd111111dcbf.
+        f66cdd1111ddc66f
+        f66ccbbbbbbcc66f
+        fcbb33333333bbcf
+        fbb3333333333bbf
+        fbb3d111111d3bbf
+        fbd1111111111dbf
+        fdd1111111111ddf
+        fdd1111111111ddf
+        fdd1111111111ddf
+        fdd1111111111ddf
+        fdd11dbbbbd11ddf
+        cdbbddddddddbbdf
+        cbddddddddddddbc
+        cddddddddddddddc
+        .cccccccccccccc.
+        .fbbfbbbbbbfbbf.
+        ..ff........ff..
+        `, SpriteKind.kagu)
+    bed.setPosition(72, 200)
+    king1 = sprites.create(img`
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 4 5 5 5 5 4 e f . . . 
+        . . f b 3 e 4 4 4 4 e 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f 3 3 f f e e e e f f 3 3 f . 
+        . f b b f b f e e f b f b b f . 
+        . f b b e 1 f 4 4 f 1 e b b f . 
+        f f b b f 4 4 4 4 4 4 f b b f f 
+        f b b f f f e e e e f f f b b f 
+        . f e e f b d d d d b f e e f . 
+        . . e 4 c d d d d d d c 4 e . . 
+        . . e f b d b d b d b b f e . . 
+        . . . f f 1 d 1 d 1 d f f . . . 
+        . . . . . f f b b f f . . . . . 
+        `, SpriteKind.king)
+    king1.setPosition(184, 56)
+    king1 = sprites.create(img`
+        . . . . f f f f f f . . . . . . 
+        . . f f e e e e f b f . . . . . 
+        . f f e e e e f b b b f . . . . 
+        . f e e e f f e e e e f . . . c 
+        . f f f f e e b b b b e f . c d 
+        . f e b b b f f f f e b f c d d 
+        f f f f f f f e e e f f c d d c 
+        f f e 4 4 e b f 4 4 e c d d c . 
+        f e e 4 d 4 1 f d d e c d c . . 
+        . f e e e 4 d d d e d c c c . . 
+        . . f f e e 4 4 e c d d e . . . 
+        . . . f b b b b c c e e . . . . 
+        . . . f b b b b e b f . . . . . 
+        . . . f c c c c 5 5 f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . f f f . . . . . . . . 
+        `, SpriteKind.king)
+    king1.setPosition(152, 68)
+    king1 = sprites.create(img`
+        . . . . . . f f f f f f . . . . 
+        . . . . . f b f e e e e f f . . 
+        . . . . f b b b f e e e e f f . 
+        c . . . f e e e e f f e e e f . 
+        d c . f e b b b b e e f f f f . 
+        d d c f b e f f f f b b b e f . 
+        c d d c f f e e e f f f f f f f 
+        . c d d c e 4 4 f b e 4 4 e f f 
+        . . c d c e d d f 1 4 d 4 e e f 
+        . . c c c d e d d d 4 e e e f . 
+        . . . e d d c e 4 4 e e f f . . 
+        . . . . e e c c b b b b f . . . 
+        . . . . . f b e b b b b f . . . 
+        . . . . . f 5 5 c c c c f . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . . f f f . . . . . 
+        `, SpriteKind.king)
+    king1.setPosition(216, 68)
+    mySprite = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    mySprite.setPosition(8, 152)
+    controller.moveSprite(mySprite, 80, 80)
+    scene.cameraFollowSprite(mySprite)
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    game.splash(blockSettings.readString("a"))
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    blockSettings.writeString("a", "test")
+})
+let mySprite: Sprite = null
+let king1: Sprite = null
+let bed: Sprite = null
+scene.setBackgroundColor(7)
+tiles.setTilemap(tilemap`レベル1`)
+spritesetting()
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// 自動生成されたコードです。変更しないでください。
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "レベル1":
+            case "レベル1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000606000000000001010101010101000006060606060600010303030303010000000000000000000103030303030100010101010101000001030303030301000104040303010000010303030303010001030303030100000103030303030100010105050101000001010303030101000000020200000000000000020000000002020202020202020202020202020200000002000000000000000000000000000001030101010000000000000000000000010303030100000000000000000000000103030301000000000000000000000001010101010000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 2 2 2 2 2 2 2 . 
+. . . . . . . . 2 . . . . . 2 . 
+. . . . . . . . 2 . . . . . 2 . 
+2 2 2 2 2 2 . . 2 . . . . . 2 . 
+2 . . . . 2 . . 2 . . . . . 2 . 
+2 . . . . 2 . . 2 . . . . . 2 . 
+2 2 2 2 2 2 . . 2 2 . . . 2 2 . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. 2 . 2 2 2 . . . . . . . . . . 
+. 2 . . . 2 . . . . . . . . . . 
+. 2 . . . 2 . . . . . . . . . . 
+. 2 2 2 2 2 . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.builtin.brick,sprites.swamp.swampTile16,sprites.swamp.swampTile9,sprites.dungeon.chestOpen,sprites.dungeon.stairLadder,sprites.swamp.swampTile3], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// 自動生成されたコードです。変更しないでください。
